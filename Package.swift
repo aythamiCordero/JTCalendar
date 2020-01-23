17a6efc3516048efeb447f21de95b3776d888785
Aythami Cordero Soto <aythami.cordero@bitboxonline.com>
2020-01-23 10:20:54 +0000

    changes in package swift

--- Package.swift
+++ Package.swift
@@ -27,8 +27,5 @@ let package = Package(
         .target(
             name: "JTCalendar",
             dependencies: []),
-        .testTarget(
-            name: "JTCalendarTests",
-            dependencies: ["JTCalendar"]),
     ]
 )
